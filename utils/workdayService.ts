4a9bc1268bdc93b92c66d9fa9d12350a1a9fe76a
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-02-23 19:10:51 -0500

    add isWorkdayX helpers to workdayService,

--- utils/workdayService.ts
+++ utils/workdayService.ts
@@ -1,10 +1,12 @@
 // ~/utils/workdayService.ts
 
-import { useQuery, useMutation } from '@tanstack/vue-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/vue-query';
 import type { WorkdayApiResponse } from 'server/api/workday';
+import { computed } from 'vue';
 
 export function workdayService() {
   const toast = useToast();
+  const queryClient = useQueryClient();
 
   // get the public apiUrl from nuxt.config.ts
   const runtimeConfig = useRuntimeConfig();
@@ -14,22 +16,28 @@ export function workdayService() {
     data: workday,
     refetch,
     isLoading,
-  } = useQuery<WorkDay, Error>({
-    queryKey: ['workday'],
+  } = useQuery<WorkDay>({
+    queryKey: ['workday_service'],
     queryFn: async (): Promise<WorkDay> => {
       try {
-        const response = await $fetch<WorkdayApiResponse>(`${apiUrl}/workday`);
+        const response = await useFetch<WorkdayApiResponse>(`${apiUrl}/workday`);
+        console.log('workday service sees workday as: ', response.data.value);
         return {
-          start_time: response.start_time ? new Date(response.start_time) : null,
-          end_time: response.end_time ? new Date(response.end_time) : null,
+          start_time: response.data.value?.start_time
+            ? new Date(response.data.value.start_time)
+            : null,
+          end_time: response.data.value?.end_time ? new Date(response.data.value.end_time) : null,
         };
       } catch (error) {
         console.error('Error fetching workday from API: ', error);
         throw error;
       }
     },
-    enabled: true, // TODO: figure out when to enable this
+    // TODO: figure out when to enable this
+    enabled: true,
+    // no need to go stale, since the only place data is updated is here
     staleTime: Number.POSITIVE_INFINITY,
+    // refetch to make sure the stopwatch doesn't get too far out of sync
     refetchInterval: 1000 * 60 * 5, // 5 minutes
   });
 
@@ -56,7 +64,7 @@ export function workdayService() {
       }
     },
     onSuccess: async (updatedWorkdayData) => {
-      await refetch();
+      queryClient.invalidateQueries({ queryKey: ['workday_service'] });
       const startTime: Date | null = updatedWorkdayData.start_time;
       const endTime: Date | null = updatedWorkdayData.end_time;
       toast.add({
@@ -70,14 +78,37 @@ export function workdayService() {
       console.error(`Failed to update workday: ${error.message}.`);
       toast.add({
         severity: 'error',
-        summary: "Uh oh",
+        summary: 'Uh oh',
         detail: 'Something went wrong when trying to update the workday.',
         life: 4000,
       });
     },
   });
 
-  return { workday, updateWorkday, isLoading, refetch };
+  const isWorkdayOpen = computed(() => {
+    return (workday.value?.start_time !== null &&
+      workday.value?.end_time === null) as boolean;
+  });
+
+  const isWorkdayClosed = computed(() => {
+    return (workday.value?.start_time !== null &&
+      workday.value?.end_time !== null) as boolean;
+  });
+
+  const isWorkdayNull = computed(() => {
+    return (workday.value?.start_time === null &&
+      workday.value?.end_time === null) as boolean;
+  });
+
+  return {
+    workday,
+    updateWorkday,
+    isLoading,
+    refetch,
+    isWorkdayNull,
+    isWorkdayClosed,
+    isWorkdayOpen,
+  };
 }
 
 export interface WorkDay {

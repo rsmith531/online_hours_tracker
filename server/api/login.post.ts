0298dc648a93599906c453ec725bc0ef7868f15e
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-03-03 22:29:54 -0500

    max session age to 1 week

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -1,28 +1,35 @@
 // ~/server/api/login.post.ts
 
-import { z } from 'zod'
+import { z } from 'zod';
 
 const bodySchema = z.object({
   email: z.string().email(),
-  password: z.string().min(8)
-})
+  password: z.string().min(8),
+});
 
 // https://nuxt.com/docs/guide/recipes/sessions-and-authentication#login-api-route
 export default defineEventHandler(async (event) => {
-  const { email, password } = await readValidatedBody(event, bodySchema.parse)
+  const { email, password } = await readValidatedBody(event, bodySchema.parse);
 
-  if (email === '2ryan.d.smith+workdaytracker@gmail.com' && password === 'DeltaEchoCharlie') {
+  if (
+    email === process.env.LOGIN_USERNAME &&
+    password === process.env.LOGIN_PASSWORD
+  ) {
     // set the user session in the cookie
     // this server util is auto-imported by the auth-utils module
-    await setUserSession(event, {
-      user: {
-        name: 'Ryan'
-      }
-    })
-    return {}
+    await setUserSession(
+      event,
+      {
+        user: {
+          name: process.env.LOGIN_NAME,
+        },
+      },
+      { maxAge: 60 * 60 * 24 * 7 } // 1 week
+    );
+    return {};
   }
   throw createError({
     statusCode: 401,
-    message: 'Bad credentials'
-  })
-})
+    message: 'Bad credentials',
+  });
+});

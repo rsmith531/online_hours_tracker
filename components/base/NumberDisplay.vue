caef372459637fca8f0d598e770cffb5ee37f37f
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-03-04 02:22:48 -0500

    solve hydration mismatch

--- components/base/NumberDisplay.vue
+++ components/base/NumberDisplay.vue
@@ -1,33 +1,35 @@
 <!-- NumberDisplay.vue -->
 
 <template>
-    <p
-      :style="{
-        border: '1px solid grey',
-        borderRadius: '0.5rem',
-        width: 'fit-content',
-        maxWidth: 'calc(100%/8)',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        transition: 'all 0.2s ease'
-      }"
-      class="
+  <p :style="{
+    border: '1px solid grey',
+    borderRadius: '0.5rem',
+    width: 'fit-content',
+    maxWidth: 'calc(100%/8)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    transition: 'all 0.2s ease'
+  }" class="
         text-3xl sm:text-8xl 
         h-[4rem] sm:h-[8rem] 
         p-[0.5rem] sm:p-[1rem]
-      "
-    >
-      {{ number }}
-    </p>
-  </template>
-  
+      ">
+    {{ isMounted ? number : '-' }}
+  </p>
+</template>
+
 <script setup>
 defineProps({
   number: {
     type: [Number, String],
     required: true,
   },
+});
+const isMounted = ref(false);
+
+onMounted(() => {
+  isMounted.value = true;
 });
 </script>

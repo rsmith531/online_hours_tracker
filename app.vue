6af5e51d6f3a662eed43c60e8ce33d0e2b69f881
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-03-15 20:20:08 -0400

    statically import css

--- app.vue
+++ app.vue
@@ -14,6 +14,7 @@
 
 <script setup lang="ts">
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
+import '~/assets/css/main.css';
 
 const runtimeConfig = useRuntimeConfig()
 // TODO: does this need to be a ref?

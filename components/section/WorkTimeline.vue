fdac694326ba44acad9c698000b1755679e25b62
rsmith531 <2ryan.d.smith+github@gmail.com>
2025-03-17 21:22:33 -0400

    convert timeline to use dates

--- components/section/WorkTimeline.vue
+++ components/section/WorkTimeline.vue
@@ -17,34 +17,57 @@ const props = defineProps({
     }
 })
 
+enum PointType {
+    Padding = 'padding',
+    Begin = 'Start',
+    Current = 'Current',
+    EstimatedEnd = 'Estimated',
+    End = 'End',
+    Pause = 'On Break',
+    Unpause = 'Off Break'
+}
+
 const timelineSvg = ref<SVGSVGElement | null>(null);
 
 // constant value for a straight line
 const yValue: number = 0;
 
-// { time in minutes, a constant value for a straight horizontal line, time description }
-const dummyData: { date: number, amount: number, type: string }[] = [
+// points to render on timeline
+const startTime: Date = new Date();
+startTime.setHours(8, 30, 0, 0); // set to 8:30a
+const startPadding: Date = new Date(startTime.getTime() - 30 * 60 * 1000);
+const currentTime: Date = new Date();
+// currentTime.setHours(14, 30, 0, 0); // set to 14:30a
+const estimatedEndTime: Date = new Date(startTime.getTime() + 8 * 60 * 60 * 1000);
+const endPadding: Date = new Date(Math.max(estimatedEndTime.getTime(), currentTime.getTime()) + 30 * 60 * 1000);
+
+const dummyData: { date: Date, type: PointType }[] = [
     // small lead value for styling
-    { date: 8 * 60, amount: yValue, type: 'padding' }, // 8:00a
+    { date: startPadding, type: PointType.Padding },
     // start work
-    { date: 8 * 60 + 30, amount: yValue, type: 'begin workday' }, // 8:30a
-    // simulate break
-    { date: 11 * 60, amount: yValue, type: 'begin break' }, // 11:00a
-    { date: 11 * 60 + 20, amount: yValue, type: 'end break' }, // 11:20a
+    { date: startTime, type: PointType.Begin },
+    // simulated breaks
+    { date: new Date(new Date().setHours(9, 0)), type: PointType.Pause }, // 9:00a
+    { date: new Date(new Date().setHours(9, 50)), type: PointType.Unpause }, // 9:50a
+    { date: new Date(new Date().setHours(11, 0)), type: PointType.Pause }, // 11:00a
+    { date: new Date(new Date().setHours(11, 20)), type: PointType.Unpause }, // 11:20a
+    // current working duration
+    { date: currentTime, type: PointType.Current },
     // simulate expected 8 hour day
-    { date: 16 * 60 + 50, amount: yValue, type: 'expected workday' }, // 4:50p
-    // actual time off work
-    { date: 18 * 60 + 22, amount: yValue, type: 'end workday' }, // 6:22p
+    { date: estimatedEndTime, type: PointType.EstimatedEnd },
     // small lag value for styling
-    { date: 18 * 60 + 22 + 30, amount: yValue, type: 'padding' }, // 6:52p
+    { date: endPadding, type: PointType.Padding },
 ];
 
 onMounted(() => {
-    console.log(`[WorkTimeline] ============================`)
+    console.log('[WorkTimeline] ============================')
     if (timelineSvg.value) {
+        console.log(`[WorkTimeline] window.innerHeight: ${window.innerHeight}`)
+        console.log(`[WorkTimeline] window.outerHeight: ${window.outerHeight}`)
+
         // instantiate the d3 chart
-        const width = props.vertical === false ? 800 : 100;
-        const height = props.vertical === false ? 100 : 800;
+        const width = props.vertical === false ? 800 : 200;
+        const height = props.vertical === false ? 100 : window.outerHeight;
         const svg = select(timelineSvg.value)
             .attr("width", width)
             .attr("height", height);
@@ -61,15 +84,15 @@ onMounted(() => {
         const x = props.vertical
             ? scaleLinear([yValue, yValue], [0 + 10, width - 10])
             : scaleLinear([minimumValue, maximumValue], [0 + 10, width - 10]);
-        // scale for the y axis
+
         const y = props.vertical
             ? scaleLinear([minimumValue, maximumValue], [20, height - 20])
             : scaleLinear([yValue, yValue], [height / 2, height / 2]);
 
-        // declare the line generator
-        const chartline = line<{ date: number, amount: number }>()
-            .x(d => props.vertical ? x(d.amount) : x(d.date))
-            .y(d => props.vertical ? y(d.date) : y(d.amount));
+        // declare the line generator with the type of an element in the dummyData array
+        const chartline = line<typeof dummyData[number]>()
+            .x(d => props.vertical ? x(yValue) : x(d.date))
+            .y(d => props.vertical ? y(d.date) : y(yValue));
 
         /**
          * function to draw a line segment between two data points.
@@ -79,8 +102,8 @@ onMounted(() => {
          * @param endIndex where the line segment should end
          * @param color what color the line segment should be
          */
-        const drawSegment = (data: { date: number, amount: number }[], startIndex: number, endIndex: number, color: string) => {
-            const segmentData = data.slice(startIndex, endIndex + 1);
+        const drawSegment = (data: typeof dummyData, startIndex: number, endIndex: number, color: string) => {
+            const segmentData = [data[startIndex], data[endIndex]];
             svg.append("path")
                 .datum(segmentData)
                 .attr("fill", "none")
@@ -91,44 +114,54 @@ onMounted(() => {
                 .attr("d", chartline(segmentData));
         };
 
-        // example drawing line segments for dummy data
-        //    active workday should be green
-        //    workday past estimated 8 hours should be red
-        //    break periods should be yellow
-        //    padding segments and the segment between current work duration and estimated workday should be var(--p-panel-color)
+        // draw the relevant line segments on the chart
 
-        // draw the initial segment
-        const beginWorkIndex = dummyData.findIndex(d => d.type === 'begin workday');
+        // draw the start padding segment
+        const beginWorkIndex = dummyData.findIndex(d => d.type === PointType.Begin);
         drawSegment(dummyData, 0, beginWorkIndex, "var(--p-panel-color)");
 
-        // draw the workday segment
-        const beginBreakIndex = dummyData.findIndex(d => d.type === 'begin break');
-        drawSegment(dummyData, beginWorkIndex, beginBreakIndex, "green");
+        // draw the workday segment to the closer value of expected or current index
+        const currentIndex = dummyData.findIndex(d => d.type === PointType.Current);
+        const expectedIndex = dummyData.findIndex(d => d.type === PointType.EstimatedEnd);
+        drawSegment(dummyData, beginWorkIndex, dummyData[expectedIndex].date.getTime() < dummyData[currentIndex].date.getTime() ? expectedIndex : currentIndex, "var(--workday-active)");
 
 
-        // draw the break segment
-        const endBreakIndex = dummyData.findIndex(d => d.type === 'end break');
-        drawSegment(dummyData, beginBreakIndex, endBreakIndex, "yellow");
+        // draw the overtime segment starting from the expected end to the current index
+        console.log(`[WorkTimeline] current date: ${dummyData[currentIndex].date}; type: ${dummyData[currentIndex].type}`)
+        console.log(`[WorkTimeline] expected date: ${dummyData[expectedIndex].date}; type: ${dummyData[expectedIndex].type}`)
+        if (dummyData[currentIndex].date > dummyData[expectedIndex].date) {
+            drawSegment(dummyData, expectedIndex, currentIndex, "var(--workday-closed)");
+        }
 
+        // draw the end padding segment
+        drawSegment(dummyData, currentIndex, dummyData.length - 1, "var(--p-panel-color)");
 
-        // draw the next workday segment
-        const expectedIndex = dummyData.findIndex(d => d.type === 'expected workday');
-        drawSegment(dummyData, endBreakIndex, expectedIndex, "green");
-
-
-        // draw the overtime segment
-        const endIndex = dummyData.findIndex(d => d.type === 'end workday');
-        drawSegment(dummyData, expectedIndex, endIndex, "red");
-
-
-        // draw the final segment
-        drawSegment(dummyData, endIndex, dummyData.length - 1, "var(--p-panel-color)");
+        // draw any paused segments
+        for (let i = 0; i < dummyData.length; i++) {
+            if (dummyData[i].type === PointType.Pause) {
+                // find the next larger point.date value
+                let nextPointIndex = -1;
+                for (let j = i + 1; j < dummyData.length; j++) {
+                    if (dummyData[j].type === PointType.Unpause) {
+                        nextPointIndex = j;
+                        break;
+                    }
+                }
+                // use it to draw a yellow segment
+                if (nextPointIndex === -1) {
+                    // draw up to the currentIndex if there is no next unpause
+                    drawSegment(dummyData, i, currentIndex, "var(--workday-paused)");
+                } else {
+                    drawSegment(dummyData, i, nextPointIndex, "var(--workday-paused)");
+                }
+            }
+        }
 
         // add tick marks for each data point
-        const tickHeight = 20; // adjust this value to control the height of the ticks
+        const tickHeight = 20;
 
         svg.selectAll("line.tick") // use a class to easily select the ticks
-            .data(dummyData.slice(1, -1)) // minus the padding data points
+            .data(dummyData.filter(d => d.type !== 'padding')) // minus the padding data points
             .enter()
             .append("line")
             .classed("tick", true) // add the class to the new lines
@@ -144,17 +177,19 @@ onMounted(() => {
             .attr("stroke-width", 3)
             .attr("stroke-linecap", "round");
 
-        // add labels for each data point (excluding padding)
-        const labelSpacing = 5; // adjust this value for spacing between label and line
+        // add labels for each data point
+        const labelSpacing = 5;
 
         svg.selectAll("text.label")
-            .data(dummyData.filter(d => d.type !== 'padding')) // don't label padding points
+            .data(dummyData.filter(d => d.type !== 'padding')) // minus the padding data points
             .enter()
             .append("text")
             .classed("label", true)
-            .attr("x", d => props.vertical
+            .attr("x", (d, i) => props.vertical
                 // vertical label position
-                ? width / 2 + tickHeight / 2 + labelSpacing // right of the line
+                ? (i % 2 === 0
+                    ? width / 2 + tickHeight / 2 + labelSpacing  // right of the line
+                    : width / 2 - tickHeight / 2 - labelSpacing - 70) // left of the line
                 : x(d.date)
             )
             .attr("y", (d, i) => props.vertical
@@ -162,14 +197,24 @@ onMounted(() => {
                 : (
                     // horizontal label position
                     i % 2 === 0
-                        ? height / 2 - tickHeight / 2 - labelSpacing - 5 // above the line
+                        ? height / 2 - tickHeight / 2 - labelSpacing - 22 // above the line
                         : height / 2 + tickHeight / 2 + labelSpacing + 15 // below the line
                 )
             )
-            .attr("dy", () => props.vertical ? "0.35rem" : 0)
+            .attr("dy", () => props.vertical ? "-0.26rem" : 0)
             .attr("text-anchor", d => props.vertical ? "start" : "middle") // center the text
             .style("fill", "var(--p-panel-color)")
-            .text(d => d.date);
+            .each(function (d) {
+                const textElement = select(this);
+                textElement.append("tspan")
+                    .text(d.type)
+                    .attr("x", textElement.attr("x"))
+
+                textElement.append("tspan")
+                    .text(d.date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }))
+                    .attr("x", textElement.attr("x"))
+                    .attr("dy", "1.2rem"); // add vertical spacing between lines
+            });
 
     } else { console.error("[WorkTimeline] SVG ref is not available.") }
 
